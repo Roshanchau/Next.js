1ee1287b55b65253b841bc8c3d281aa533f1d0de
Roshan Chaudhary <87389059+Roshanchau@users.noreply.github.com>
2023-06-01 16:06:06 +0545

    fetching data getStaticProps

--- src/pages/ninjas/index.js
+++ src/pages/ninjas/index.js
@@ -1,11 +1,31 @@
 // since it is the index.js the route for this is /ninjas only since for index.js the route is it's route.
+import styles from "../../styles/Ninjas.module.css"
 
-const Ninjas = () => {
+// so with this getStaticprops asynchronous function we are trying to fetch the dummy api data from the jsonPlaceHolder
+// which returns the props as the data being fetched.
+export const getStaticProps=async()=>{
+    const res=await fetch("https://jsonplaceholder.typicode.com/users");
+    const data=await res.json();
+
+    return{
+        props:{ninjas:data}
+    }
+}
+
+//here we have use {ninjas} to destructure the props being returend
+const Ninjas = ({ninjas}) => {
     return ( 
         <div>
             <h1>
                 ALL Ninjas
             </h1>
+            {ninjas.map(ninja=>(
+                <div key={ninja.id}>
+                    <a className={styles.single}>
+                        <h3>{ninja.name}</h3>
+                    </a>
+                </div>
+            ))}
         </div>
      );
 }
